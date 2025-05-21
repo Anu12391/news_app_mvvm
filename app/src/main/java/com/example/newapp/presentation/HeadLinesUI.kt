a9282b6626877392d9841a442933a4d6925608a7
Anu <anuavr123@gmail.com>
2025-05-21 18:47:16 +0530

    added headline list item

--- app/src/main/java/com/example/newapp/presentation/HeadLinesUI.kt
+++ app/src/main/java/com/example/newapp/presentation/HeadLinesUI.kt
@@ -2,13 +2,19 @@ package com.example.newapp.presentation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newapp.domain.model.ArticlesItem
+import com.example.newapp.domain.model.HeadlineResponseDTO
 
 @Composable
 fun HeadLinesUI(headLineViewModel: HeadlinesViewmodel = hiltViewModel()) {
@@ -31,7 +37,17 @@ fun HeadLinesUI(headLineViewModel: HeadlinesViewmodel = hiltViewModel()) {
     res.data?.let {
 
         LazyColumn {
-           // items(it)
+            items(it) {
+               // HeadlineListItem(it.articles.)
+            }
         }
+    }
+}
+
+
+@Composable
+fun HeadlineListItem(articleItem: ArticlesItem) {
+    Card(modifier = Modifier.padding(8.dp)) {
+        Text(text = articleItem.title)
     }
 }
